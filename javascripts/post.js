cdc1d1e37863a7930092a0bc6b48edcb06df4e01
tiankonguse <i@tiankonguse.com>
2014-10-23 22:13:13 +0800

    Update post.js

--- javascripts/post.js
+++ javascripts/post.js
@@ -137,7 +137,7 @@ jQuery(document).ready(function(){
             };
         })();
 
-        if($('.entry h2').length > 0 && !isMobile.any() && !ie6){
+        if($('.entry h2').length > 0 &&  !ie6){
 
             genIndex();
 
