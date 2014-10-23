b13fdde217d070744ce1303e9a187ccbba018e72
tiankonguse <i@tiankonguse.com>
2014-10-23 19:05:44 +0800

    Update post.js

--- javascripts/post.js
+++ javascripts/post.js
@@ -137,7 +137,7 @@ jQuery(document).ready(function(){
             };
         })();
 
-        if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
+        if($('.entry h2').length > 0 && !isMobile.any() && !ie6){
 
             genIndex();
 
