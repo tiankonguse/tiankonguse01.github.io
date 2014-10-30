491b520107a5deb4ac711edfe269ecffc9cab675
tiankonguse <i@tiankonguse.com>
2014-10-30 13:30:11 +0800

    update post js to fixed

--- javascripts/post.js
+++ javascripts/post.js
@@ -133,7 +133,7 @@ jQuery(document).ready(function(){
         })();
 
         function getNowTop(index, nowTop){
-            bottomHeight = $("#content .entry").height() + $("#content .entry-header").height() + 150;
+            bottomHeight = $("#content .entry").height() + $("#content .entry-header").height() + 190;
             var mytop  = nowTop + shouldPos - scrollLiTop[index];
 
             if(mytop  > nowTop){
