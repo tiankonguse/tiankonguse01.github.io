f5eb035a82c0df0792ea654aaa320b0a9dfc76db
tiankonguse <i@tiankonguse.com>
2015-03-18 00:09:18 +0800

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -682,7 +682,7 @@ jQuery(document).ready(function(){
 });
 
 function loadSidebar(){
-    $(".home-menu-ex, sidebar-close").click(function(){
+    $(".home-menu-ex, sidebar-close,close-icon").click(function(){
         $("body").toggleClass("sidebar-visible");
     });
 }
