257b3f0f31f3d96ebe138b64c09695a2a0d6965b
tiankonguse <i@tiankonguse.com>
2015-09-04 11:51:20 +0800

    remove ad

--- javascripts/tk.js
+++ javascripts/tk.js
@@ -531,8 +531,8 @@ tk.Composition(TK, {
 */
 tk.AddMethod(TK,{
     AD : function AD(){
-        this.isShowPageFoot = true;
-        this.isLoadGoogleJs = true;
+        this.isShowPageFoot = false;
+        this.isLoadGoogleJs = false;
         this.alreadyLoad = false;
         this.adList = [];
         this.endText = '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
