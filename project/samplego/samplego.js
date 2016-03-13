50e4a2ad1059747577baf443096c82d20dea21b5
tiankonguse <i@tiankonguse.com>
2016-03-14 01:26:55 +0800

    update samplego

--- project/samplego/samplego.js
+++ project/samplego/samplego.js
@@ -174,6 +174,23 @@ tk.Composition(TK.SampleGo, {
         }
         return tk.makePoint(x, y);
     },
+    "play": function(point){
+        if(point.x < 1 || point.x > that.boardLev){
+            return;
+        }
+        if(point.y < 1 || point.y > that.boardLev){
+            return;
+        }
+        point.x--;
+        point.y--;
+        
+        if(this.boardMap[point.x][point.y]){
+            return;
+        }
+        
+        
+        
+    },
     "initPiece" : function(){
         var that = this;
         this.pieceCxt = this.canvas.getCanvas(this.pieceDom);
@@ -181,6 +198,10 @@ tk.Composition(TK.SampleGo, {
 	        var point = that.getPos(e.offsetX, e.offsetY);
 	        if (!point){
 	            return;
+	        }
+	        if(that.play(point)){
+        	    this.drawEmptyBoard();
+	            this.drawPiece();
 	        }
         });
         this.pieceDom.bind("mousemove", function(e){
@@ -794,37 +815,8 @@ function stone_down(row, col) {
 	move_record.push([row, col, move_count]);	// 记录手数
 }
 
-function grid(cxt) {
-	// the first point is (30, 30)
-	for (var i = 0; i < 19; i++) {
-		cxt.beginPath();
-		cxt.moveTo(0+30,   (i+1)*30);
-		cxt.lineTo(600-30, (i+1)*30);
-		cxt.stroke();
-	}
-	for (var i = 0; i < 19; i++) {
-		cxt.beginPath();
-		cxt.moveTo((i+1)*30,   0+30);
-		cxt.lineTo((i+1)*30, 600-30);
-		cxt.stroke();
-	}
 
-}
-function ninePoints(cxt) {
-	var np = new Array(
-		[120,120],[300,120],[480,120],
-		[120,300],[300,300],[480,300],
-		[120,480],[300,480],[480,480]
-	);
-	
-	for (var i = 0; i < np.length; i++) {
-		//circle
-		cxt.beginPath();
-		cxt.arc(np[i][0],np[i][1],3,0,2*Math.PI,false);
-		cxt.fillStyle="black";
-		cxt.fill();
-	}
-}
+
 
 
 var move_count = 0;
@@ -862,74 +854,10 @@ function mousedownHandler(e) {
 	showPan();
 }
 
-function mousemoveHandler(e) {
-	var x, y;
-	if (e.offsetX || e.offsetX == 0) {
-		x = e.offsetX ;
-		y = e.offsetY ;
-	}
-	if (x < 30-10 || x > 600-30+10)
-		return;
-	if (y < 30-10 || y > 600-30+10)
-		return;
-	
-	var xok = false;
-	var yok = false;
-	for (var i = 1; i <= 19; i++) {
-		if (x > i*30-15 && x < i*30+15) {
-			x = i*30;
-			xok = true;
-		}
-		if (y > i*30-15 && y < i*30+15) {
-			y = i*30;
-			yok = true;
-		}
-	}
-	if (!xok || !yok)
-		return;
-
-	var c = document.getElementById("path");
-	var cxt = c.getContext("2d");
-
-	// clear the path
-	cxt.clearRect(0,0,600,600);
-
-	// put a new Gray stone
-	cxt.beginPath();
-	cxt.arc(x,y,15,0,2*Math.PI,false);
-	cxt.fillStyle="gray";
-	cxt.fill();
-
-	cxt.beginPath();
-	cxt.arc(x,y,10,0,2*Math.PI,false);
-	if (move_count % 2 == 0)
-		cxt.fillStyle="black";
-	else
-		cxt.fillStyle="white";
-	cxt.fill();
-}
-
-function mouseoutHandler(e) {
-	var c = document.getElementById("path");
-	var cxt = c.getContext("2d");
-	cxt.clearRect(0,0,600,600);
-}
-
 function initBoard() {
 	var c_path = document.getElementById("path");
 	c_path.addEventListener('mousedown', mousedownHandler, false);
-	c_path.addEventListener('mousemove', mousemoveHandler, false);
-	c_path.addEventListener('mouseout', mouseoutHandler, false);
 
-	var c_weiqi = document.getElementById("weiqi-board");
-	var cxt = c_weiqi.getContext("2d");
-	cxt.fillStyle = "silver";
-	cxt.fillRect(0,0,600,600);
-
-	grid(cxt);
-	ninePoints(cxt);
-
-	showPan();
 }
 
 function addLoadEvent(func) {
